19c41c2625e4bb492a560eb1620b2832479397fc
Luis Andrade <luisandrade195@gmail.com>
2021-11-08 01:50:32 -0300

    Navbar mejorado

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {CartWidget} from "../CartWidget/CartWidget";
 import {Container, Nav, Navbar} from 'react-bootstrap';
 import  './NavBar.css'
+import logo from './logo.PNG'
 
 export const NavBar = () => {
 
@@ -10,6 +11,7 @@ export const NavBar = () => {
         <header>
             <Navbar bg="light" variant="light">
                 <Container>
+                    <img src={logo} alt={"pic"}/>
                     <Navbar.Brand href="#home">Candle Cake</Navbar.Brand>
                     <Nav className="me-auto">
                         <Nav.Link href="#home">Inicio</Nav.Link>
